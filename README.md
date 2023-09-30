d5c71e9120e80d94e5bae0f4a17f0c13095d36b7
yifengyou <842056007@qq.com>
2023-09-30 11:02:03 +0800

    修正样式

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 </head>
-
 <div align="center">
     <div style="width: fit-content; margin-left: auto; margin-right: auto;">
     <img src="https://github-readme-stats.vercel.app/api?username=yifengyou&theme=default&hide_border=true&show_icons=true&hide_title=true" width="49%" />
--- main.py
+++ main.py
@@ -16,15 +16,13 @@ TOKEN_FILE = "/etc/yifengyou"
 CONTENT = """
 
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 </head>
-
 <div align="center">
     <div style="width: fit-content; margin-left: auto; margin-right: auto;">
     <img src="https://github-readme-stats.vercel.app/api?username=yifengyou&theme=default&hide_border=true&show_icons=true&hide_title=true" width="49%" />
     <img src="https://github-readme-streak-stats.herokuapp.com/?user=yifengyou&theme=default&hide_border=true" alt="Streak Stats" width="49%" />
     </div>
-    
     <img src="https://user-images.githubusercontent.com/19882390/203891125-3d50010e-dc87-45a9-a53c-d53ff21c3e4b.png" width="25%"  alt="微信" title="微信"/>
     <img src="https://user-images.githubusercontent.com/19882390/203893616-72e4b570-2628-4067-980b-a73aab253c7b.png" width="25%"  alt="钉钉" title="钉钉"/>
     <br><br>
