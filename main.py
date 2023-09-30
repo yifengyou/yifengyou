7aa16ccbd32e948c97bda2188a1ca946eae979bb
yifengyou <842056007@qq.com>
2023-09-30 10:57:37 +0800

    调整样式

--- main.py
+++ main.py
@@ -18,18 +18,18 @@ CONTENT = """
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 </head>
+
 <div align="center">
-<div style="width: fit-content; margin-left: auto; margin-right: auto;">
-<img src="https://github-readme-stats.vercel.app/api?username=yifengyou&theme=default&hide_border=true&show_icons=true&hide_title=true" width="49%" />
-<img src="https://github-readme-streak-stats.herokuapp.com/?user=yifengyou&theme=default&hide_border=true" alt="Streak Stats" width="49%" />
-</div>
-
-<img src="https://user-images.githubusercontent.com/19882390/203891125-3d50010e-dc87-45a9-a53c-d53ff21c3e4b.png" width="25%"  alt="微信" title="微信"/>
-<img src="https://user-images.githubusercontent.com/19882390/203893616-72e4b570-2628-4067-980b-a73aab253c7b.png" width="25%"  alt="钉钉" title="钉钉"/>
-
-  <br><br>
-  <strong>技术方向：操作系统、内核、虚拟化、云原生 :blush: 欢迎交流~ :blush: </strong>
-  <br><br>
+    <div style="width: fit-content; margin-left: auto; margin-right: auto;">
+    <img src="https://github-readme-stats.vercel.app/api?username=yifengyou&theme=default&hide_border=true&show_icons=true&hide_title=true" width="49%" />
+    <img src="https://github-readme-streak-stats.herokuapp.com/?user=yifengyou&theme=default&hide_border=true" alt="Streak Stats" width="49%" />
+    </div>
+    
+    <img src="https://user-images.githubusercontent.com/19882390/203891125-3d50010e-dc87-45a9-a53c-d53ff21c3e4b.png" width="25%"  alt="微信" title="微信"/>
+    <img src="https://user-images.githubusercontent.com/19882390/203893616-72e4b570-2628-4067-980b-a73aab253c7b.png" width="25%"  alt="钉钉" title="钉钉"/>
+    <br><br>
+    <strong>技术方向：操作系统、内核、虚拟化、云原生 :blush: 欢迎交流~ :blush: </strong>
+    <br><br>
     <a href="https://github.com/yifengyou">
       <img src="https://badges.strrl.dev/visits/yifengyou/yifengyou?style=flat-square&color=black&logo=github">
     </a>
@@ -43,8 +43,6 @@ CONTENT = """
       <img src="https://badges.strrl.dev/commits/monthly/yifengyou?style=flat-square&color=black&logo=github">
     </a>
 </div>
-
-<h1></h1>
 
 **语言/框架**
 
@@ -337,6 +335,7 @@ def handle_generate_profile(args):
   </tbody>
 </table>
 
+</html>
 """
 
     template = Template(template)
